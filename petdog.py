2c59eb4213d5c7c68daa7fd69be07c487ab50829
Milad <milad.hajihassan@gmail.com>
2017-07-22 17:01:44 -0700

    no message

--- petdog.py
+++ petdog.py
@@ -11,7 +11,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
 
-while(1):
+def getch():
     import sys, tty
     init()
     # Take each frame
@@ -20,9 +20,10 @@ while(1):
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Define range of yellow color in HSV for tennis ball
-    lower_yellow = np.array([30, 255, 135], dtype=np.uint8)
-    upper_yellow = np.array([40, 255, 185], dtype=np.uint8)
+    # define range of yellow color in HSV for tennis ball
+    lower_yellow = np.array([30, 60, 165], dtype=np.uint8)
+    upper_yellow = np.array([40, 120, 255], dtype=np.uint8)
+
 
     # Threshold the HSV image to get only yellow colors
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
@@ -31,28 +32,33 @@ while(1):
     area = moments['m00']
 
     if(area > 100000): 
-        #Determine the x and y coordinates of the center of the object 
+        #determine the x and y coordinates of the center of the object 
         #we are tracking by dividing the 1, 0 and 0, 1 moments by the area 
         x = moments['m10'] / area
         y = moments['m01'] / area
 
         print 'x: ' + str(int(x)) + ' y: ' + str(int(y)) + ' area: ' + str(area)
         frame = cv2.circle(frame,(int(x), int(y)), 2, (255, 255, 255), 20)
-        move_pet(x,y,area)
-
+        move_pet(round(x, -1),y,round(area, -5))
+        time.sleep(0.15)
+    
+    #cv2.imshow('mask',mask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         clean_up()
-        break
+        sys.exit()
 
-cv2.destroyAllWindows()
+
 
 
 def move_pet(x,y,area):
-    if x > 320
-       pivot_right((x-320)/(320))
-    elif x < 320
-       pivot_left((320-x)/(320))
-  
+    if x > 330:
+       pivot_right((x-320)/(320*12))
+    elif x < 310:
+       pivot_left((320-x)/(320*12))
+    elif ((310 <= x <= 330) and area<1500000):
+       forward(0.05)
+    elif ((310 <= x <= 330) and area>4000000):
+       reverse(0.05)
 
 def init():
     gpio.setmode(gpio.BOARD)
@@ -121,3 +127,14 @@ def clean_up():
     gpio.output(15, False)
     gpio.output(16, False)
     gpio.cleanup()   
+
+def main():
+    while True:
+        getch()
+    cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    main()
+
